5cc6cdb4ce42808b315979ccb289535c0d3302d1
dhawandj <112755140+dhawandj@users.noreply.github.com>
2023-06-29 18:22:09 +0530

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,9 @@
+print("dhawan")
+if(10<20):
+    print("its true ")
+    if(10>1):
+        print("10 is grater 1 is true")
+    else:
+        print("10 is false")    
+else:
+    print("its false")    
